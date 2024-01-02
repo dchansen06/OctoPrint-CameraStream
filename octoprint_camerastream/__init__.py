3e1df3d358654d3d3ee08cc275bb33cae5ed7852
dchansen06 <105574022+dchansen06@users.noreply.github.com>
2024-01-02 14:58:54 -0800

    Update __init__.py

--- octoprint_camerastream/__init__.py
+++ octoprint_camerastream/__init__.py
@@ -48,7 +48,10 @@ class CameraStreamPlugin(octoprint.plugin.StartupPlugin,
 	def on_api_get(self, request):
 		self._logger.info(request.args);
 		if "mjpg" in request.args or "stream" in request.args:	
-			response = flask.Response(self._stream_as_bytes(), mimetype = "multipart/x-mixed-replace; boundary=frame");
+			response = flask.Response(flask.stream_with_context(
+				self._stream_as_bytes(),
+				mimetype = "multipart/x-mixed-replace; boundary=frame"
+			))
 		elif "snapshot" in request.args or "jpg" in request.args:
 			response = flask.make_response(self._snapshot_as_bytes());
 			response.headers["Content-Type"] = "image/jpg";
