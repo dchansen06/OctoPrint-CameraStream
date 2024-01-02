fae72290b6a3d40f1591b34c250916dc8cbb6e48
dchansen06 <105574022+dchansen06@users.noreply.github.com>
2024-01-02 12:00:39 -0800

    Create __init__.py

---
+++ octoprint_camerastream/__init__.py
@@ -0,0 +1,103 @@
+import octoprint.plugin;
+import octoprint.schema.webcam;
+import flask;
+import cv2;
+import multiprocessing;
+import time;
+
+class CameraStreamPlugin(octoprint.plugin.StartupPlugin,
+						octoprint.plugin.SimpleApiPlugin,
+						octoprint.plugin.WebcamProviderPlugin):
+
+	vid = cv2.VideoCapture(0);
+
+	def _snapshot_as_bytes(self):
+		self._logger.info("Snapshotting");
+		if not self.vid.isOpened():
+			return bytes("Cannot open", "utf-8");
+		self.vid.read();
+		ret, frame = self.vid.read();
+		if not ret:
+			return bytes("Cannot read", "utf-8");
+		ret, buffer = cv2.imencode(".jpg", frame);
+		if not ret:
+			return bytes("Cannot convert", "utf-8");
+		return buffer.tobytes();
+
+	def on_after_startup(self):
+		self._logger.info("Configuring camera stream");
+
+		self._logger.info("Did stuff");
+		self.vid.set(cv2.CAP_PROP_BUFFERSIZE, 1);
+		self.vid.set(cv2.CAP_PROP_FRAME_WIDTH, 640);
+		self.vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 480);
+
+		if not self.vid.isOpened():
+			self._logger.info("Never opened");
+
+	def get_api_commands(self):
+		return dict();
+	def on_api_command(self, command, data):
+		self._logger.info("Something happened {command} and {data}");
+	def on_api_get(self, request):
+		self._logger.info(request.args);
+		response = flask.make_response(self._snapshot_as_bytes());
+		response.headers["Content-Type"] = "image/jpg";
+		return response;
+
+	def get_webcam_configurations(self):
+		return [
+			octoprint.schema.webcam.Webcam(
+				name = "camerastream",
+				displayName = "Camera Stream",
+				canSnapshot = True,
+				snapshot = "Internal Camera Stream",
+				compat = octoprint.schema.webcam.WebcamCompatibility(
+					snapshot = "/plugin/camerastream?snapshot",
+				)
+			)
+		]
+
+	def take_webcam_snapshot(self):
+		return self._snapshot_as_bytes();
+
+	def gut_update_information(self):
+		return dict(
+			camerastream = dict(
+				displayName = f"{self._plugin_name} Plugin",
+				displayVersion = self._plugin_version,
+				type = "github_release",
+				user = "dchansen06",
+				repo = "OctoPrint-CameraStream",
+				current = self._plugin_version,
+				pip = "https://github.com/dchansen06/OctoPrint-CameraStream/archive/{target_version}.zip",
+			)
+		)
+
+__plugin_name__ = "Camera Stream";
+__plugin_pythoncompat__ = ">=3.7,<4";
+__plugin_implementation__ = CameraStreamPlugin();
+__plugin_hooks = {
+	"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+}
+
+#import cv2;
+#vid = cv2.VideoCapture(0);
+#vid.set(cv2.CAP_PROP_FRAME_WIDTH, 640);
+#vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 480);
+#
+#while(True):
+#	if not vid.isOpened():
+#		break;
+#
+#	ret, frame = vid.read();
+#
+#	if not ret:
+#		break;
+#
+#	cv2.imshow("Camera Frame", frame);
+#	if cv2.waitKey(1) & 0xFF == ord('q'):
+#		break;
+#
+#vid.release();
+#cv2.destroyAllWindows();
