d776e0a3f8f5f71a50d34b2cc01074e2fde32658
dchansen06 <105574022+dchansen06@users.noreply.github.com>
2024-01-02 17:39:30 -0800

    Update camerastream.js

--- octoprint_camerastream/static/js/camerastream.js
+++ octoprint_camerastream/static/js/camerastream.js
@@ -1,1 +1,1 @@
-setInterval(document.getElementById("webcam_image").src = document.getElementById("webcam_image").src, 1.0 / OctoPrintClient.settings.getPluginSettings(camerastream, fps));
+setInterval(document.getElementById("webcam_image").src = document.getElementById("webcam_image").src, 1.0 / OctoPrint.settings.getPluginSettings("camerastream", "fps"));
